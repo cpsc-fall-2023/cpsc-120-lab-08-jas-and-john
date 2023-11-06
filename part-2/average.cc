634cbdebf42d90bf60dbf14a8ce9c100ba03c799
Jonathan Diep <jonathandiep@Jonathans-MacBook-Air-8.local>
2023-11-06 13:48:46 -0800

    Attempting part-2

--- part-2/average.cc
+++ part-2/average.cc
@@ -9,23 +9,23 @@
 
 int main(int argc, char* argv[]) {
   std::vector<std::string> arguments{argv, argv + argc};
-  std::vector<double> values;
-  double sum{0};
 
-  if (arguments.size() <= 1) {
+  if (arguments.size() <= 2) {
     std::cout << "error: you must supply at least one number \n";
     return 1;
   }
-
-  for (int i = 1; i < argc; i++) {
-    values.push_back(std::stod(arguments[i]));
-  }
-
-  for (double element  : values) {
-    sum += element;
+  
+  double sum{0};
+  for (const std::string& num : arguments) {
+    if (num == arguments.at(0)) {
+      continue;
+    }
+    double number{std::stod(num)};
+    sum += number;
   }
 
   double average{sum / (argc - 1)};
+
   std::cout << "average = " << average << "\n";
   return 0;
 }
